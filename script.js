bcb0e8a319de3587e1e897143c5f62fd40e2d1dc
G186742021 <103623728+G186742021@users.noreply.github.com>
2024-11-07 17:02:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,24 +13,7 @@ function initMap() {
     // マーカーの座標とマーカーの全天球画像
     const markerData = [
         { position: { lat: 35.62996668120076, lng: 139.2799270710072 }, page: 'sky1.html' },
-        { position: { lat: 35.62977993057464, lng: 139.28073285916975 }, page: 'sky2.html' },
-        { position: { lat: 35.629082899737014, lng: 139.28052898505635 }, page: 'sky/sky3.html' },
-        { position: { lat: 35.628154392599505, lng: 139.28016654219047 }, page: 'sky/sky4.html' },
-        { position: { lat: 35.62738632756379, lng: 139.27987852955098 }, page: 'sky/sky5.html' },
-        { position: { lat: 35.62698124918482, lng: 139.27976526615663 }, page: 'sky/sky6.html' },
-        { position: { lat: 35.627470499420845, lng: 139.27943842035575 }, page: 'sky/sky7.html' },
-        { position: { lat: 35.6275862355979, lng: 139.27905008871113 }, page: 'sky/sky8.html' },
-        { position: { lat: 35.62857524702214, lng: 139.2794028232984 }, page: 'sky/sky9.html' },
-        { position: { lat: 35.62917496076154, lng: 139.2796164057157 }, page: 'sky/sky10.html' },
-        { position: { lat: 35.62698142656696, lng: 139.27881723383808 }, page: 'sky/sky11.html' },
-        { position: { lat: 35.62623176312509, lng: 139.27852922116347 }, page: 'sky/sky12.html' },
-        { position: { lat: 35.625550483940714, lng: 139.2783059304547 }, page: 'sky/sky13.html' },
-        { position: { lat: 35.625787222535884, lng: 139.27740629543123 }, page: 'sky/sky14.html' },
-        { position: { lat: 35.62702614309579, lng: 139.27795319584024 }, page: 'sky/sky15.html' },
-        { position: { lat: 35.62589243946781, lng: 139.27897903865062 }, page: 'sky/sky16.html' },
-        { position: { lat: 35.6267420611002, lng: 139.279176440588 }, page: 'sky/sky17.html' },
-        { position: { lat: 35.62609498165998, lng: 139.28005989517257 }, page: 'sky/sky18.html' },
-        { position: { lat: 35.625195374721535, lng: 139.27974599372604 }, page: 'sky/sky19.html' },
+        { position: { lat: 35.62977993057464, lng: 139.28073285916975 }, page: 'sky2.html' }
     ];
 
   
